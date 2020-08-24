de6e4db2f3e9c2edd9ef4b2299cde7c06a671ad3
RyLaney <rylaney@gmail.com>
2020-08-23 21:58:10 -0700

    fixes typo.

--- userpass/command_line.py
+++ userpass/command_line.py
@@ -7,5 +7,5 @@ import userpass.generate
 def main():
     """main function
     """
-    print(f'ACESS_KEY={userpass.generate.token_urlsafe(32)}')
+    print(f'ACCESS_KEY={userpass.generate.token_urlsafe(32)}')
     print(f'SECRET_KEY={userpass.generate.token_urlsafe(64)}')
